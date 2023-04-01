934e4b963a7d7b1e8e40729f0cc14cb29ea9c1b2
Ahn Daeyeong <ahndaeyeong@naver.com>
2023-04-01 16:18:23 +0900

    백준 2178번 미로
    완성본

--- src/baekjoon/N_2178.java
+++ src/baekjoon/N_2178.java
@@ -32,11 +32,52 @@ public class N_2178 {
 		
 		while(queueX.isEmpty()==false)
 		{
+			x=queueX.poll();
+			y=queueY.poll();
 			
+			if(x>0&&arr[0][y][x-1]==1)
+			{
+				if(arr[1][y][x-1]==0)
+				{
+					arr[1][y][x-1]=arr[1][y][x]+1;
+					queueY.add(y);
+					queueX.add(x-1);
+				}
+			}
+			
+			if(x<m-1&&arr[0][y][x+1]==1)
+			{
+				if(arr[1][y][x+1]==0)
+				{
+					arr[1][y][x+1]=arr[1][y][x]+1;
+					queueY.add(y);
+					queueX.add(x+1);
+				}
+			}
+			
+			if(y>0&&arr[0][y-1][x]==1)
+			{
+				if(arr[1][y-1][x]==0)
+				{
+					arr[1][y-1][x]=arr[1][y][x]+1;
+					queueY.add(y-1);
+					queueX.add(x);
+				}
+			}
+			
+			if(y<n-1&&arr[0][y+1][x]==1)
+			{
+				if(arr[1][y+1][x]==0)
+				{
+					arr[1][y+1][x]=arr[1][y][x]+1;
+					queueY.add(y+1);
+					queueX.add(x);
+				}
+			}
 		}
 		
 
-		
+		System.out.println(arr[1][n-1][m-1]+1);
 		  scan.close();
 		
 
